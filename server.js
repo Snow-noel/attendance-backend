db75aef37016a404aa96c46aa895f31641e30c4e
Snowden Msumba <bis23-smsumba@mubas.ac.mw>
2026-05-28 18:15:44 +0200

    added authentication - register, login and JWT middleware

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const pool = require("./database");
 
-const express= require('express');
+const bcrypt = require('bcrypt');
 
+const jsonwebtoken = require('jsonwebtoken');
+
+const verifyToken= require("./malware")
+
+const express= require('express');
 
 const app= express();
 
@@ -40,7 +45,7 @@ app.post("/session/start", async(req, res) => {
         res.status(500).json({ message: "Error starting session", error: err.message})
     }});
 
-app.post("/attendance/mark", async(req, res) => {
+app.post("/attendance/mark", verifyToken, async(req, res) => {
     const { student_id, session_code } = req.body;
 
    
@@ -74,4 +79,64 @@ const sessionResult = await pool.query(
 }catch(err){
     res.status(500).json({message:"error while marking the attendance",error:err})
 }
+
 });
+
+
+app.post("/student/register", async(req, res) => {
+    const {registration_number, first_name, last_name, email, password, program_id}= req.body;
+
+    try{
+        
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        const result = await pool.query(
+            `INSERT INTO students(registration_number, first_name, last_name, email, password, program_id) 
+             VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`, [registration_number,first_name,last_name,email,hashedPassword,program_id]
+        )
+
+        res.json({
+            message: "student registered successifully",
+            student:result.rows[0]
+        });
+    }catch (err){
+        res.status(500).json({message: "error while registering a student", error: err})
+    }
+});
+
+app.post("/student/login", async(req, res) => {
+    const {email,password}= req.body
+
+    try{
+        const result = await pool.query(
+            `SELECT * FROM students WHERE email = $1`,[email]
+        )
+
+        if(result.rows.length==0){
+            return res.status(404).json({message: "Student not found"})
+        }
+
+        const student = result.rows[0];
+
+        const comparePassword = await bcrypt.compare(password, student.password);
+
+        if(!comparePassword){
+            return res.status(401).json({message: "incorrect password"})
+        }
+
+
+        const token = jsonwebtoken.sign(
+            {id: student.id, email: student.email, role: "student"},
+            process.env.JWT_SECRET,
+            {expiresIn: "8h"}
+        );
+
+        res.json({
+            message: "Student login successifully",
+            token: token
+
+        })
+    }catch (err){
+        res.status(500).json({message: "error while logging in ", error:err})
+    }
+})
